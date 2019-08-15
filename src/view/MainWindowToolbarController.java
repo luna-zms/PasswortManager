0c4dea0780da8045f031cc30233171df4660cf9d
Sebastian Schmidt <profekaris.sebastian@gmail.com>
2019-08-15 22:58:07 +0200

    #8 Updated the view controller to contain all icons

--- src/view/MainWindowToolbarController.java
+++ src/view/MainWindowToolbarController.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuButton;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
@@ -33,7 +34,10 @@ public class MainWindowToolbarController extends GridPane {
 	private Button exportDatabaseToolbar;
 	
 	@FXML
-	private Button searchButtonToolbar;
+	private Button searchButtonSearchbar;
+
+	@FXML
+	private MenuButton filterMenuSearchbar;
 	
 	public MainWindowToolbarController() {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainWindowToolbar.fxml"));
@@ -46,8 +50,32 @@ public class MainWindowToolbarController extends GridPane {
 			e.printStackTrace();
 		}
 		
-		Image addEntryImage = new Image(getClass().getResourceAsStream("add_category_toolbar_icon.png"));
-		
+		Image addEntryImage = new Image(getClass().getResourceAsStream("add_entry_toolbar_icon.png"));
 		addEntryToolbar.setGraphic(new ImageView(addEntryImage));
-	}	
+
+		Image addCategoryImage = new Image(getClass().getResourceAsStream("add_category_toolbar_icon.png"));
+		addCategoryToolbar.setGraphic(new ImageView(addCategoryImage));
+
+		Image saveDatabaseImage = new Image(getClass().getResourceAsStream("save_toolbar_icon.png"));
+		saveDatabaseToolbar.setGraphic(new ImageView(saveDatabaseImage));
+
+		Image openDatabaseImage = new Image(getClass().getResourceAsStream("open_toolbar_icon.png"));
+		openDatabaseToolbar.setGraphic(new ImageView(openDatabaseImage));
+
+		Image setMasterPwImage = new Image(getClass().getResourceAsStream("change_master_password_toolbar_icon.png"));
+		setMasterPasswordToolbar.setGraphic(new ImageView(setMasterPwImage));
+
+		Image importImage = new Image(getClass().getResourceAsStream("import_toolbar_icon.png"));
+		importDatabaseToolbar.setGraphic(new ImageView(importImage));
+
+		Image exportImage = new Image(getClass().getResourceAsStream("export_toolbar_icon.png"));
+		exportDatabaseToolbar.setGraphic(new ImageView(exportImage));
+
+
+		Image searchButtonImage = new Image(getClass().getResourceAsStream("search_icon.png"));
+		searchButtonSearchbar.setGraphic(new ImageView(searchButtonImage));
+
+		Image filterButtonImage = new Image(getClass().getResourceAsStream("filter_icon.png"));
+		filterMenuSearchbar.setGraphic(new ImageView(filterButtonImage));
+	}
 }
