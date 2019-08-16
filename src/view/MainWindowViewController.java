5a948a311b9390dc2ad9c6be11db50be48debc5d
Simon Engmann <simon.engmann@tu-dortmund.de>
2019-08-16 11:52:14 +0200

    Add basic view controller class

--- src/view/MainWindowViewController.java
+++ src/view/MainWindowViewController.java
@@ -1,4 +1,36 @@
 package view;
 
-public class MainWindowViewController {
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
+
+public class MainWindowViewController extends BorderPane {
+    @FXML // fx:id="entryNamePreview"
+    private Label entryNamePreview; // Value injected by FXMLLoader
+
+    @FXML // fx:id="usernamePreview"
+    private Label usernamePreview; // Value injected by FXMLLoader
+
+    @FXML // fx:id="urlPreview"
+    private Label urlPreview; // Value injected by FXMLLoader
+
+    @FXML // fx:id="validUntilPreview"
+    private Label validUntilPreview; // Value injected by FXMLLoader
+
+    @FXML // fx:id="tagListPreview"
+    private ListView<?> tagListPreview; // Value injected by FXMLLoader
+
+    public MainWindowViewController() {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/MainWindowView.fxml"));
+        loader.setRoot(this);
+        loader.setController(this);
+        try {
+            loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
