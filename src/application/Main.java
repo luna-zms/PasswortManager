10a27a15389c22eea487de50ebd143556729e662
Simon Engmann <simon.engmann@tu-dortmund.de>
2019-08-16 13:10:57 +0200

    Unbreak main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,17 +1,17 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import view.SampleViewController;
+import view.MainWindowViewController;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			SampleViewController sampleViewController = new SampleViewController();
-			Scene scene = new Scene(sampleViewController,400,200);
+			MainWindowViewController mainWindowViewController = new MainWindowViewController();
+			Scene scene = new Scene(mainWindowViewController);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
@@ -19,7 +19,7 @@ public class Main extends Application {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
