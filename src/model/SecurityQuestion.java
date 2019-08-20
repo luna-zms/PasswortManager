5ed9987aecab1c6e8ddcc6127d21a3ca33884e01
Simon Engmann <simon.engmann@tu-dortmund.de>
2019-08-20 15:53:52 +0200

    Add model classes for Entry and SecurityQuestion

---
+++ src/model/SecurityQuestion.java
@@ -0,0 +1,26 @@
+package model;
+
+public class SecurityQuestion {
+    private String question, answer;
+
+    public SecurityQuestion(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+}
