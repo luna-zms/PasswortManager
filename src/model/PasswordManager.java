e002774a7d79866d513be4b170e20e77a108a870
sopr016 sopra <sopr016@fiws148.cs.tu-dortmund.de>
2019-08-21 14:23:14 +0200

    #46 Fixes and JavaDoc in PasswordManager and SecurityQuestion

--- src/model/PasswordManager.java
+++ src/model/PasswordManager.java
@@ -2,18 +2,69 @@ package model;
 
 import javax.crypto.SecretKey;
 import java.time.LocalDateTime;
+
+import java.util.ArrayList;
 import java.util.List;
 
-public class PasswordManager {
+import javax.crypto.SecretKey;
 
+/**
+ * This class is used to save the database dependent application data, e.g. the hashed master password.
+ * @author sopr016
+ *
+ */
+public class PasswordManager {
     private SecretKey masterPasswordKey;
 
     private LocalDateTime lastModified;
 
     private LocalDateTime validUntil;
 
-    private Tag tag;
+    private Tag rootTag;
 
-    private List<Entry> entry;
+    private List<Entry> entries;
 
+    public PasswordManager(){
+    	entries = new ArrayList<>();
+    }
+
+	public SecretKey getMasterPasswordKey() {
+		return masterPasswordKey;
+	}
+
+	public void setMasterPasswordKey(SecretKey masterPasswordKey) {
+		this.masterPasswordKey = masterPasswordKey;
+	}
+
+	public LocalDateTime getLastModified() {
+		return lastModified;
+	}
+
+	public void setLastModified(LocalDateTime lastModified) {
+		this.lastModified = lastModified;
+	}
+
+	public LocalDateTime getValidUntil() {
+		return validUntil;
+	}
+
+	public void setValidUntil(LocalDateTime validUntil) {
+		this.validUntil = validUntil;
+	}
+
+	public Tag getTag() {
+		return rootTag;
+	}
+
+	public void setTag(Tag tag) {
+		this.rootTag = tag;
+	}
+
+	public List<Entry> getEntries() {
+		return entries;
+	}
+
+	public void setEntries(List<Entry> entries) {
+		this.entries = entries;
+	}
 }
--- src/model/SecurityQuestion.java
+++ src/model/SecurityQuestion.java
@@ -1,8 +1,17 @@
 package model;
 
+/**
+ * This class is about setting privacy issues
+ * @author sopr016
+ *
+ */
 public class SecurityQuestion {
     private String question, answer;
 
+    /**
+     * @param question the value to be asked
+     * @param answer   the value to be answered
+     */
     public SecurityQuestion(String question, String answer) {
         this.question = question;
         this.answer = answer;
