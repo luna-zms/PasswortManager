88f6e2943b53b88ae35b7267d59123d89b11bec9
Simon Engmann <simon.engmann@tu-dortmund.de>
2019-08-21 12:35:12 +0200

    Add sample entry for table

--- src/view/MainWindowViewController.java
+++ src/view/MainWindowViewController.java
@@ -1,16 +1,25 @@
 package view;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import controller.PMController;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.BorderPane;
+import model.Entry;
 import model.Tag;
 
 public class MainWindowViewController extends BorderPane {
     private PMController pmController;
 
+    @FXML // fx:id="entryPreview"
+    private EntryPreviewViewController entryPreview; // Value injected by FXMLLoader
 
     @FXML // fx:id="entryTable"
     private EntryListViewController entryTable; // Value injected by FXMLLoader
@@ -36,8 +45,23 @@ public class MainWindowViewController extends BorderPane {
 
         // TODO: Fetch from model
         Tag rootTag = new Tag("Root Tag");
-
         tagTree.init(true, rootTag);
+
+        // TODO: Fetch from model
+        List<Entry> entries = new ArrayList<>();
+        Entry entry = new Entry("Hello there", "asdf");
+        entry.setUsername("userSample");
+
+        URL url = null;
+        try {
+            url = new URL("http://example.com");
+        } catch (MalformedURLException e) {}
+
+        entry.setUrl(url);
+        entry.setValidUntil(LocalDateTime.MAX);
+
+        entries.add(entry);
+        entryTable.setEntries(FXCollections.observableList(entries));
     }
 
     public void setPmController(PMController pmController) {
