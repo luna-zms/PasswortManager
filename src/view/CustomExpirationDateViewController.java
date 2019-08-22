d2765d52a842c88ead0729550ab1ff84989cefa5
Felix Buchheit <sopr015.sopra@cs.uni-dortmund.de>
2019-08-22 10:55:29 +0200

    Implemented basic funcionality of CustomExpirationDate-Widget

--- src/view/CustomExpirationDateViewController.java
+++ src/view/CustomExpirationDateViewController.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.binding.Bindings;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
@@ -11,9 +12,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 import javafx.scene.layout.GridPane;
 import java.time.LocalDateTime;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 
 public class CustomExpirationDateViewController extends GridPane {
 
@@ -37,30 +40,56 @@ public class CustomExpirationDateViewController extends GridPane {
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/CustomExpirationDate.fxml"));
         loader.setRoot(this);
         loader.setController(this);
+        
         try {
             loader.load();
         } catch (IOException e) {
 
             e.printStackTrace();
         }
+       
+        daysUntilExpiration.setValueFactory(new SpinnerValueFactory.IntegerSpinnerValueFactory(0, Integer.MAX_VALUE));
     }
 
     @FXML
     void initialize() {
-        checkBoxExpirationDate.setSelected(true);
-        /*
-        checkBoxExpirationDate.selectedProperty().addListener(new ChangeListener<Boolean>() {
-            @Override
-            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
-            	checkBoxExpirationDate.setSelected(!newValue);
-            }
+        checkBoxExpirationDate.setSelected(false);
+        datePickerExpirationDate.setDisable(true);
+		daysUntilExpiration.setDisable(true);
+        
+        //datePickerExpirationDate.setValue(LocalDate.MIN.plusDays(100));
+        
+        
+        datePickerExpirationDate.setOnAction(event ->{
+        	if (event.getSource() instanceof DatePicker) {
+        		LocalDate date = datePickerExpirationDate.getValue();
+        		if (!LocalDate.now().isAfter(date)){
+        			int noOfDaysBetween = (int) ChronoUnit.DAYS.between(LocalDate.now(), date);
+        			SpinnerValueFactory<Integer> temp = daysUntilExpiration.getValueFactory();
+        			temp.setValue(noOfDaysBetween);
+        		}
+        	}
         });
         
-        */
+        daysUntilExpiration.valueProperty().addListener((obs, oldValue, newValue) -> {
+            datePickerExpirationDate.setValue(LocalDate.now().plusDays(newValue));
+        });
+        
+        /*datePickerExpirationDate.valueProperty().bind(Bindings.createObjectBinding(
+        	() -> LocalDate.now().plusDays(daysUntilExpiration.getValue(),
+        	daysUntilExpiration.valueProperty()
+        ));*/
         
         checkBoxExpirationDate.setOnAction(event -> {
         	if (event.getSource() instanceof CheckBox) {
-        		
+        		if(checkBoxExpirationDate.isSelected()){
+        			
+        			datePickerExpirationDate.setDisable(false);
+        			daysUntilExpiration.setDisable(false);
+        		}else{
+        			datePickerExpirationDate.setDisable(true);
+        			daysUntilExpiration.setDisable(true);
+        		}
             }
         });
     }
