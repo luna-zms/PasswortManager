d99b34975873d653d6fc168a34e0a3062ac13d7d
Patrick Roy <patrick.roy@tu-dortmund.de>
2019-08-22 11:09:41 +0200

    Merge branch 'origin/47-SerializationController' of https://sopra-gitlab.cs.tu-dortmund.de/sopra19B/gruppe01/projekt1 into 47-SerializationController
    # Conflicts:
    #	src/controller/SerializationController.java:q

--- src/controller/SerializationController.java
+++ src/controller/SerializationController.java
@@ -4,12 +4,48 @@ import java.io.OutputStream;
 import java.util.List;
 
 import model.Entry;
+import model.SecurityQuestion;
 import model.Tag;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public abstract class SerializationController {
 
     protected PMController pmController;
+
+    private enum EntryTableHeader {
+        TITLE, USERNAME, PASSWORD, CREATEDAT, LASTMODIFIED, VALIDUNTIL, NOTE, SECURITYQUESTION, SECURITYQUESTIONANSWER, TAGPATHS;
+
+        @Override
+        public String toString() {
+            switch (this) {
+                case TITLE: return "Title";
+                case USERNAME: return "Username";
+                case PASSWORD: return "Password";
+                case CREATEDAT: return "CreatedAt";
+                case LASTMODIFIED: return "LastModified";
+                case VALIDUNTIL: return "ValidUntil";
+                case NOTE: return "Note";
+                case SECURITYQUESTION: return "SecurityQuestion";
+                case SECURITYQUESTIONANSWER: return "SecurityQuestionAnswer";
+                case TAGPATHS: return "TagPaths";
+                default: return ""; // Note: Never used. This switch is exhaustive
+            }
+        }
+    }
+
+    private static final DateTimeFormatter dateFormat = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
 
     public abstract void load(String path);
 
@@ -19,25 +55,85 @@ public abstract class SerializationController {
     public abstract void save(String path);
 
     /**
-     * @param path
-     * @return
+     * Creates the Tag pointed to by path, constructing parent Tags if needed
+     *
+     * @param path A path in the Tag tree, as an array of tag names
+     * @return The Tag pointed to by path
      */
     protected Tag createTagFromPath(String[] path) {
-        return null;
+        Tag currentTag = pmController.getPasswordManager().getTag();
+        for (String part : path) {
+            if (currentTag.hasSubTag(part)) {
+                currentTag = currentTag.getSubTags().stream().filter(t -> t.getName().equals(part)).findFirst().get();
+            } else {
+                Tag t = new Tag(part);
+                currentTag.getSubTags().add(t);
+                currentTag = t;
+            }
+        }
+        return currentTag;
     }
 
     /**
-     *
+     * Writes all entries stored in the PasswordManager into the given OutputStream
+     * @param os The OutputStream to write into
      */
-    protected void writeEntriesToStream(OutputStream os) {
+    protected void writeEntriesToStream(OutputStream os) throws IOException {
+        List<Entry> entries = pmController.getPasswordManager().getEntries();
+        Map<Tag, String> pathMap = pmController.getPasswordManager().getTag().createPathMap();
 
+        CSVPrinter printer = new CSVPrinter(new OutputStreamWriter(os), CSVFormat.DEFAULT);
+
+        // Print Header
+        printer.printRecord((Object[]) EntryTableHeader.values());
+        // Print Entries
+        for (Entry entry : entries) {
+            String paths = entry.getTags().stream().map(pathMap::get).collect(Collectors.joining(";"));
+            printer.printRecord(
+                    entry.getTitle(),
+                    entry.getUsername(),
+                    entry.getPassword(),
+                    entry.getUrl(),
+                    entry.getCreatedAt().format(dateFormat),
+                    entry.getLastModified().format(dateFormat),
+                    entry.getValidUntil().format(dateFormat),
+                    entry.getNote(),
+                    entry.getSecurityQuestion().getQuestion(),
+                    entry.getSecurityQuestion().getAnswer(),
+                    paths
+            );
+        }
     }
 
     /**
-     *
+     * Parses CSVRecords into Entries. Entries are added to the password manager. Tags are added to the tag tree.
+     * @param csvEntries CSV records to parse.
      */
-    protected List<Entry> parseEntries(Iterable<CSVRecord> csvEntries) {
-        return null;
-    }
+    protected void parseEntries(Iterable<CSVRecord> csvEntries) {
+        List<Entry> entries = pmController.getPasswordManager().getEntries();
+        for (CSVRecord record : csvEntries) {
+            Entry entry = new Entry(record.get(EntryTableHeader.TITLE), record.get(EntryTableHeader.PASSWORD));
 
+            entry.setUsername(record.get(EntryTableHeader.USERNAME));
+            entry.setCreatedAt(LocalDateTime.parse(record.get(EntryTableHeader.CREATEDAT), dateFormat));
+            entry.setLastModified(LocalDateTime.parse(record.get(EntryTableHeader.LASTMODIFIED), dateFormat));
+            entry.setValidUntil(LocalDateTime.parse(record.get(EntryTableHeader.VALIDUNTIL), dateFormat));
+            entry.setNote(record.get(EntryTableHeader.NOTE));
+            String question = record.get(EntryTableHeader.SECURITYQUESTION);
+            String answer = record.get(EntryTableHeader.SECURITYQUESTIONANSWER);
+            SecurityQuestion securityQuestion = new SecurityQuestion(question, answer);
+            entry.setSecurityQuestion(securityQuestion);
+
+            String tagPaths = record.get(EntryTableHeader.TAGPATHS);
+            String[] paths = tagPaths.split(";");
+            entry.getTags().addAll(
+                    Arrays.stream(paths)
+                            .map(p -> p.split("\\\\"))
+                            .map(this::createTagFromPath)
+                            .collect(Collectors.toList())
+            );
+
+            entries.add(entry);
+        }
+    }
 }
