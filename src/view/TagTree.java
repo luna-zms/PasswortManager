e198040814a32608c49ee35eb1b98e1d4e3c027b
Patrick Roy <patrick.roy@tu-dortmund.de>
2019-08-22 16:24:59 +0200

    Fixed NullPointerException with TagTree in No-Checkbox-Mode

--- src/view/TagTree.java
+++ src/view/TagTree.java
@@ -1,13 +1,13 @@
 package view;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
 import model.Tag;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TagTree extends TreeView<Tag> {
     public void init(final boolean hasCheckBoxes, Tag rootTag) {
@@ -190,8 +190,9 @@ public class TagTree extends TreeView<Tag> {
                 if (tag.getName().isEmpty()) {
                     setToTextField();
                 } else {
+                    if (checkbox != null) checkbox.setSelected(((TagTreeItem) getTreeItem()).isChecked());
+
                     setText(tag.getName());
-                    checkbox.setSelected(((TagTreeItem) getTreeItem()).isChecked());
                     setGraphic(checkbox);
                 }
             }
