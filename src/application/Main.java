9bcb0f355ec30fc81c6f99e91041a72bd14d1901
Simon Engmann <simon.engmann@tu-dortmund.de>
2019-08-22 14:45:44 +0200

    Move test data to Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,15 @@
 package application;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.LocalDate;
+
 import controller.*;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import model.Entry;
+import model.PasswordManager;
+import model.Tag;
 import util.WindowFactory;
 import view.MainWindowViewController;
 
@@ -18,6 +25,10 @@ public class Main extends Application {
     public void init() {
         pmController = new PMController();
         pmController.setPasswordController(new PasswordController());
+        PasswordManager passwordManager = new PasswordManager(null);
+        // TODO: Replace with actually loading the data
+        initSampleData(passwordManager);
+        pmController.setPasswordManager(passwordManager);
     }
 
     @Override
@@ -25,10 +36,40 @@ public class Main extends Application {
         try {
             MainWindowViewController mainWindowViewController = new MainWindowViewController();
             mainWindowViewController.setPmController(pmController);
+            mainWindowViewController.init();
             primaryStage.setScene(WindowFactory.createScene(mainWindowViewController));
             primaryStage.show();
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private void initSampleData(PasswordManager passwordManager) {
+        Tag rootTag = new Tag("Root Tag");
+        Tag subTag = new Tag("Subtag");
+        Tag subSubTag = new Tag("Subsubtag");
+        subTag.getSubTags().add(subSubTag);
+        rootTag.getSubTags().add(subTag);
+
+        passwordManager.setRootTag(rootTag);
+
+        Entry entry = new Entry("Hello there", "asdf");
+        entry.getTags().add(rootTag);
+        entry.getTags().add(subSubTag);
+        entry.setUsername("userSample");
+
+        URL url = null;
+        try {
+            url = new URL("http://example.com");
+        } catch (MalformedURLException e) {
+        }
+
+        entry.setUrl(url);
+        entry.setValidUntil(LocalDate.MAX);
+
+        passwordManager.getEntries().add(entry);
+
+        entry = new Entry("General Kenobi", "urabold1");
+        passwordManager.getEntries().add(entry);
     }
 }
--- src/view/MainWindowViewController.java
+++ src/view/MainWindowViewController.java
@@ -1,19 +1,13 @@
 package view;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 import controller.PMController;
-import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.BorderPane;
 import model.Entry;
-import model.Tag;
 
 public class MainWindowViewController extends BorderPane {
     private PMController pmController;
@@ -42,27 +36,11 @@ public class MainWindowViewController extends BorderPane {
 
         // Bind preview to update when table selection changes
         entryPreview.entryProperty().bind(entryTable.getSelectionModel().selectedItemProperty());
+    }
 
-        // TODO: Fetch from model
-        Tag rootTag = new Tag("Root Tag");
-        tagTree.init(true, rootTag);
-
-        // TODO: Fetch from model
-        List<Entry> entries = new ArrayList<>();
-        Entry entry = new Entry("Hello there", "asdf");
-        entry.setUsername("userSample");
-
-        URL url = null;
-        try {
-            url = new URL("http://example.com");
-        } catch (MalformedURLException e) {
-        }
-
-        entry.setUrl(url);
-        entry.setValidUntil(LocalDateTime.MAX);
-
-        entries.add(entry);
-        entryTable.setEntries(FXCollections.observableList(entries));
+    public void init() {
+        tagTree.init(false, pmController.getPasswordManager().getRootTag());
+        entryTable.setEntries(pmController.getPasswordManager().entriesObservable());
     }
 
     public void setPmController(PMController pmController) {
