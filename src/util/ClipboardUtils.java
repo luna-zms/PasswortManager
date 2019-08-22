83877ff88f2f80c7e973d434c20e5867f10dd36e
Simon Engmann <simon.engmann@tu-dortmund.de>
2019-08-22 23:00:25 +0200

    Add util class to copy a string into the clipboard

---
+++ src/util/ClipboardUtils.java
@@ -0,0 +1,24 @@
+package util;
+
+import java.util.function.Function;
+
+import javafx.beans.value.ObservableValue;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+
+public class ClipboardUtils {
+    private ClipboardUtils() {
+    }
+
+    public static void copyToClipboard(String str) {
+        ClipboardContent content = new ClipboardContent();
+        content.putString(str);
+
+        Clipboard.getSystemClipboard().setContent(content);
+    }
+
+    public static <T> void copyToClipboard(ObservableValue<T> observableValue, Function<T, String> getter) {
+        T value = observableValue.getValue();
+        if (value != null) copyToClipboard(getter.apply(value));
+    }
+}
