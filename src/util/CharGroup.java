656ac269a8b137ec907a5920f8690e015823896e
Alexander Korn <alexander@korn-online.org>
2019-08-23 10:35:08 +0200

    Removed safeDistinctCount

--- src/util/CharGroup.java
+++ src/util/CharGroup.java
@@ -1,22 +1,19 @@
 package util;
 
 public enum CharGroup {
-    LOWER_CASE_LETTER("abcdefghijklmnopqrstuvwxyz".toCharArray(), 10),
-    UPPER_CASE_LETTER("ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray(), 3),
-    NUMBERS("0123456789".toCharArray(), 2),
-    SPECIAL_CHARS("!\"§$%&/()=?{[]}\\+*~'#-_|<>^°".toCharArray(), 2),
-    OTHER(new char[0], 0);
+    LOWER_CASE_LETTER("abcdefghijklmnopqrstuvwxyz".toCharArray()),
+    UPPER_CASE_LETTER("ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray()),
+    NUMBERS("0123456789".toCharArray()),
+    SPECIAL_CHARS("!\"§$%&/()=?{[]}\\+*~'#-_|<>^°".toCharArray()),
+    OTHER(new char[0]);
 
     private char[] chars;
-    private int safeDistinctCount;
 
-    CharGroup(char[] chars, int safeDistinctCount) {
+    CharGroup(char[] chars) {
         this.chars = chars;
-        this.safeDistinctCount = safeDistinctCount;
     }
 
     public char[] getChars() { return chars; }
-    public int getSafeDistinctCount() { return safeDistinctCount; }
 
     public static CharGroup getCharGroupOf(char c) {
         for (CharGroup charGroup : CharGroup.values()) {
