ed73dde03dcf613f6629dd026fed55b4ae0bd09a
Patrick Roy <patrick.roy@tu-dortmund.de>
2019-08-23 22:33:15 +0200

    Implemented LoadSaveController.save(String)

--- src/controller/LoadSaveController.java
+++ src/controller/LoadSaveController.java
@@ -1,5 +1,19 @@
 package controller;
 
+import model.PasswordManager;
+import model.Tag;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.NoSuchPaddingException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 /**
  * Controller for loading and saving the program data in a save file
  * <p>
@@ -33,8 +47,60 @@ public class LoadSaveController extends SerializationController {
      *
      * @param path The file to save to
      */
+    @SuppressWarnings("PMD.ExcessiveMethodLength")
     public void save(String path) {
+        PasswordManager passwordManager = pmController.getPasswordManager();
 
+        try (FileOutputStream fos = new FileOutputStream(path); ZipOutputStream zos = new ZipOutputStream(fos)) {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, passwordManager.getMasterPasswordKey());
+
+            try (CipherOutputStream cos = createEncryptedZipEntry(zos, cipher, "MAGIC"); PrintWriter writer = new PrintWriter(cos)) {
+                writer.println("siroD");
+                writer.println(passwordManager.getLastModified().format(SerializationController.DATE_FORMAT));
+                writer.println(passwordManager.getValidUntil().format(SerializationController.DATE_FORMAT));
+            } catch (IOException e) {
+                // Error creating/writing new ZipEntry for metadata/magic header
+                e.printStackTrace();
+                return;
+            }
+
+            Tag rootTag = passwordManager.getRootTag();
+
+            try (CipherOutputStream cos = createEncryptedZipEntry(zos, cipher, "ENTRIES")) {
+                writeEntriesToStream(cos, passwordManager.getEntries(), rootTag);
+            } catch (IOException e) {
+                // Error creating/writing new ZipEntry for entries section
+                e.printStackTrace();
+                return;
+            }
+
+            try (CipherOutputStream cos = createEncryptedZipEntry(zos, cipher, "TAGS"); PrintWriter writer = new PrintWriter(cos)) {
+                if (rootTag != null)
+                    rootTag.createPathMap().values().forEach(writer::println);
+            } catch (IOException e) {
+                // Error creating/writing new ZipEntry for tags section
+                e.printStackTrace();
+            }
+        } catch (NoSuchPaddingException | NoSuchAlgorithmException e) {
+            // The java installation does not support AES encryption for some reason
+            e.printStackTrace();
+            System.exit(-1);
+        } catch (InvalidKeyException e) {
+            // Internal error: The master password key was invalid, meaning there was an error in the cdf!
+            e.printStackTrace();
+            System.exit(-1);
+        } catch (IOException e) {
+            // Error while accessing the file (path to file didn't exist, missing write permissions, etc)
+            e.printStackTrace();
+        }
+    }
+
+    private CipherOutputStream createEncryptedZipEntry(ZipOutputStream zos, Cipher cipher, String entryName) throws IOException {
+        ZipEntry zipEntry = new ZipEntry(entryName);
+        zos.putNextEntry(zipEntry);
+
+        return new CipherOutputStream(zos, cipher);
     }
 
 }
