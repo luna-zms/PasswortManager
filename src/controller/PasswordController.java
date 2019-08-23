34dbd358c4023f7c36f5bbae58bcfd6d700cb682
Simon Engmann <simon.engmann@tu-dortmund.de>
2019-08-23 14:28:53 +0200

    Change names in JavaDoc to IDs

--- src/controller/PasswordController.java
+++ src/controller/PasswordController.java
@@ -14,14 +14,12 @@ public class PasswordController {
     }
 
     /**
-     *
      * Method which checks a password for its quality. The returned double is an value between 0
      * and 1. The closer the double is to 1, the higher is the quality of the password.
      *
-     * @author Daniel Ginter
-     * @author Alexander Korn
      * @param pwd password which has to be checked for its quality
      * @return a double between 0 - 1 which indicates the passwordquality
+     * @author sopr012, sopr011
      */
     public double checkPasswordQuality(String pwd) {
         return PasswordQualityUtil.getNormalizedScore(pwd);
