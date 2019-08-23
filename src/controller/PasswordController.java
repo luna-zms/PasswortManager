76eafd7aec8b9a10acb40862edee4e153ac4d5b1
Daniel Ginter <44347258+Speick98@users.noreply.github.com>
2019-08-23 13:25:36 +0200

    Add JavaDoc to checkPasswordQuality()

--- src/controller/PasswordController.java
+++ src/controller/PasswordController.java
@@ -15,6 +15,13 @@ public class PasswordController {
 
     /**
      *
+     * Method which checks a password for its quality. The returned double is an value between 0
+     * and 1. The closer the double is to 1, the higher is the quality of the password.
+     *
+     * @author Daniel Ginter
+     * @author Alexander Korn
+     * @param pwd password which has to be checked for its quality
+     * @return a double between 0 - 1 which indicates the passwordquality
      */
     public double checkPasswordQuality(String pwd) {
         return PasswordQualityUtil.getNormalizedScore(pwd);
