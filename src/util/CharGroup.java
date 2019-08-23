c132d34dbfcd3c8e717b46256ded4c22341e9106
Alexander Korn <alexander@korn-online.org>
2019-08-23 10:15:02 +0200

    Javadoc and refactoring

--- src/util/CharGroup.java
+++ src/util/CharGroup.java
@@ -1,12 +1,13 @@
 package util;
 
 public enum CharGroup {
-    LOWER_CASE_LETTER("abcdefghijklmnopqrstuvwxyz".toCharArray(), 10), UPPER_CASE_LETTER(
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray(), 3), NUMBERS("0123456789".toCharArray(),
-            2), SPECIAL_CHARS("!\"§$%&/()=?{[]}\\+*~'#-_|<>^°".toCharArray(), 2);
+    LOWER_CASE_LETTER("abcdefghijklmnopqrstuvwxyz".toCharArray(), 10),
+    UPPER_CASE_LETTER("ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray(), 3),
+    NUMBERS("0123456789".toCharArray(), 2),
+    SPECIAL_CHARS("!\"§$%&/()=?{[]}\\+*~'#-_|<>^°".toCharArray(), 2),
+    OTHER(new char[0], 0);
 
     private char[] chars;
-
     private int safeDistinctCount;
 
     CharGroup(char[] chars, int safeDistinctCount) {
@@ -14,11 +15,14 @@ public enum CharGroup {
         this.safeDistinctCount = safeDistinctCount;
     }
 
-    public char[] getChars() {
-        return chars;
-    }
+    public char[] getChars() { return chars; }
+    public int getSafeDistinctCount() { return safeDistinctCount; }
 
-    public int getSafeDistinctCount() {
-        return safeDistinctCount;
+    public static CharGroup getCharGroupOf(char c) {
+        for (CharGroup charGroup : CharGroup.values()) {
+            if (charGroup.getChars().toString().indexOf(c) != -1) return charGroup;
+        }
+
+        return OTHER;
     }
 }
--- src/util/PasswordQualityUtil.java
+++ src/util/PasswordQualityUtil.java
@@ -3,12 +3,18 @@ package util;
 /**
  * Utils for checking the quality of a password
  *
- * @author Daniel Ginter
- * @author Alexander Korn
+ * @author sopr011, sopr012
  */
 public class PasswordQualityUtil {
     /**
-     * Get the internal score from 0-20 of a password
+     * <p>Get the internal score from 0-20 of a password by adding all partial scores together.</p>
+     * <p>Partial scores exist for the following criteria:
+     * <ul>
+     *     <li>The length of a password</li>
+     *     <li>The partitioning into different character groups of a password</li>
+     *     <li>The amount of different character groups in a password</li>
+     *     <li>The existence of commonly used words from a dictionary in a password</li>
+     * </ul></p>
      *
      * @param pwd The password to check the quality of
      * @return The internal score (0-20)
@@ -31,15 +37,27 @@ public class PasswordQualityUtil {
      * @return The normalized score (0-1)
      */
     public static double getNormalizedScore(String pwd) {
-        return (double)getScore(pwd)/20.0;
+        return (double)getScore(pwd) / 20.0;
     }
 
+    /**
+     * Returns the internal score a password gets for its length (0-5)
+     *
+     * @param pwdLength The length of a password
+     * @return The internal score (0-5)
+     */
     private static int lengthScore(int pwdLength) {
         pwdLength = Math.min(25, pwdLength);
 
         return pwdLength/5;
     }
 
+    /**
+     * Returns the internal score for the partitioning of a password into character groups (0-10)
+     *
+     * @param pwd The password to get the partitioning score for
+     * @return The internal score (0-10)
+     */
     private static int partitionScore(String pwd) {
         int partitions = 1;
         char[] pwdCharArray = pwd.toCharArray();
@@ -48,15 +66,21 @@ public class PasswordQualityUtil {
         for (int i = 1; i < pwdCharArray.length; i++) {
             char currentChar = pwdCharArray[i];
 
-            if (getGroupOfChar(currentChar) != getGroupOfChar(lastChar)) {
+            if (CharGroup.getCharGroupOf(currentChar) != CharGroup.getCharGroupOf(lastChar)) {
                 partitions += 1;
                 lastChar = currentChar;
             }
         }
 
-        return (int) (((double) partitions / (double) pwd.length()) * 10.0); // Normalize partition count
+        return (int) (((double)partitions / (double)pwd.length()) * 10.0); // Normalize partition count
     }
 
+    /**
+     * Returns the internal score for the existence of char groups in a password (0-5)
+     *
+     * @param pwd
+     * @return
+     */
     private static int charGroupsExistScore(String pwd) {
         int score = 0;
 
@@ -72,21 +96,5 @@ public class PasswordQualityUtil {
         }
 
         return score;
-    }
-
-    private static CharGroup getGroupOfChar(char c) {
-        for (CharGroup charGroup : CharGroup.values()) {
-            if (contains(charGroup.getChars(), c)) return charGroup;
-        }
-
-        throw new IllegalArgumentException("Char not in any group: " + c);
-    }
-
-    private static boolean contains(char[] chars, char cc) {
-        for (char c : chars) {
-            if (c == cc) return true;
-        }
-
-        return false;
     }
 }
