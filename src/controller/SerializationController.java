d4dc9e6fd0c3106c514d6b968d4dfdc2e4b15b84
Markus Mügge <sopr014.sopra@cs.uni-dortmund.de>
2019-08-24 14:59:35 +0200

    Correct null value handling

--- src/controller/SerializationController.java
+++ src/controller/SerializationController.java
@@ -83,15 +83,15 @@ public abstract class SerializationController {
         // Print Entries
         for (Entry entry : entries) {
             String paths = entry.getTags().stream().map(pathMap::get).collect(Collectors.joining(";"));
-            // NOTE: Url and validUntil can be null in entries. All other fields are initialized.
+            // NOTE: null values are written as empty strings. We do not need to special case null values here
             printer.printRecord(
                     entry.getTitle(),
                     entry.getUsername(),
                     entry.getPassword(),
-                    entry.getUrl() == null ? "" : entry.getUrl(),
+                    entry.getUrl(),
                     entry.getCreatedAt().format(DATE_FORMAT),
                     entry.getLastModified().format(DATE_FORMAT),
-                    entry.getValidUntil() == null ? "" : entry.getValidUntil().format(DATE_FORMAT),
+                    entry.getValidUntil(),
                     entry.getNote(),
                     entry.getSecurityQuestion().getQuestion(),
                     entry.getSecurityQuestion().getAnswer(),
@@ -119,33 +119,59 @@ public abstract class SerializationController {
                 throw new RuntimeException("Malformed CSV: Inconsistent number of records in row");
             }
 
-            Entry entry = new Entry(record.get(EntryTableHeader.title), record.get(EntryTableHeader.password));
+            // null values are written and read as empty Strings
 
+            // String attributes
+            // Since entries are initiated with empty strings, we can just write the read Strings back
+            Entry entry = new Entry(record.get(EntryTableHeader.title), record.get(EntryTableHeader.password));
             entry.setUsername(record.get(EntryTableHeader.username));
-            entry.setCreatedAt(LocalDateTime.parse(record.get(EntryTableHeader.createdAt), DATE_FORMAT));
-            entry.setLastModified(LocalDateTime.parse(record.get(EntryTableHeader.lastModified), DATE_FORMAT));
             entry.setNote(record.get(EntryTableHeader.note));
+
             String question = record.get(EntryTableHeader.securityQuestion);
             String answer = record.get(EntryTableHeader.securityQuestionAnswer);
             SecurityQuestion securityQuestion = new SecurityQuestion(question, answer);
             entry.setSecurityQuestion(securityQuestion);
 
-            // NOTE: validUntil and Url can both be null.
-            // All other entry properties are always initialized.
-            String validUntil = record.get(EntryTableHeader.validUntil);
-            if (validUntil != null) {
-                entry.setValidUntil(LocalDateTime.parse(validUntil, DATE_FORMAT));
-            }
+            // Non String attributes
+            // After initialization these attributes have a non empty string representation.
+            // Such, if we read an empty string we keep the default value, otherwise we parse the string into
+            // an object
 
-            String url = record.get(EntryTableHeader.url);
-            if (validUntil != null) {
+            String createdAt = record.get(EntryTableHeader.createdAt);
+            if (!createdAt.isEmpty()) {
                 try {
-                    entry.setUrl(new URL(url));
-                } catch (MalformedURLException e) {
-                    throw new RuntimeException("Malformed CSV: Malformed URL");
+                    entry.setCreatedAt(LocalDateTime.parse(createdAt, DATE_FORMAT));
+                } catch (DateTimeParseException exc) {
+                    throw new RuntimeException("Malformed CSV: Invalid Date format");
                 }
             }
 
+            String lastModified = record.get(EntryTableHeader.lastModified);
+            if (!createdAt.isEmpty()) {
+                try {
+                    entry.setLastModified(LocalDateTime.parse(lastModified, DATE_FORMAT));
+                } catch (DateTimeParseException exc) {
+                    throw new RuntimeException("Malformed CSV: Invalid Date format");
+                }
+            }
+
+            String validUntil = record.get(EntryTableHeader.validUntil);
+            if (!validUntil.isEmpty()) {
+                try {
+                    entry.setValidUntil(LocalDateTime.parse(validUntil, DATE_FORMAT));
+                } catch (DateTimeParseException exc) {
+                    throw new RuntimeException("Malformed CSV: Invalid Date format");
+                }
+            }
+
+            String url = record.get(EntryTableHeader.url);
+            if (!url.isEmpty()) {
+                try {
+                    entry.setUrl(new URL(url));
+                } catch (MalformedURLException exc) {
+                    throw new RuntimeException("Malformed CSV: Malformed URL");
+                }
+            }
 
             String tagPaths = "build_root\\".concat(record.get(EntryTableHeader.tagPaths));
             String[] paths = tagPaths.split(";");
