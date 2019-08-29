ad97e027b95845d3259256b5d4261f398cafb485
Simon Engmann <simon.engmann@tu-dortmund.de>
2019-08-29 16:43:49 +0200

    Remove SuppressWarnings and fix exc bubbling

--- src/controller/LoadSaveController.java
+++ src/controller/LoadSaveController.java
@@ -37,7 +37,6 @@ import java.util.zip.ZipOutputStream;
  * De-/Encryption is done using standard AES. Each entry of the `.zip` file will be encrypted individually, the file as
  * a whole will not be encrypted
  */
-@SuppressWarnings({"PMD.ExcessiveMethodLength", "PMD.CyclomaticComplexity"})
 public class LoadSaveController extends SerializationController {
     public LoadSaveController(PasswordManager passwordManager) {
         super(passwordManager);
@@ -226,12 +225,12 @@ public class LoadSaveController extends SerializationController {
 
             try (CipherOutputStream cos = createEncryptedZipEntry(zos, cipher, "TAGS"); PrintWriter writer = new PrintWriter(cos)) {
                 rootTag.createPathMap().values().forEach(writer::println);
-            } catch (IOException e) {
+            } catch (IOException exception) {
                 // Error creating/writing new ZipEntry for tags section
-                e.printStackTrace();
-                throw e;
+                exception.printStackTrace();
+                throw exception;
             }
-        } catch (NoSuchPaddingException | InvalidKeyException | IOException | NoSuchAlgorithmException exception) {
+        } catch (NoSuchPaddingException | InvalidKeyException | NoSuchAlgorithmException exception) {
             // The java installation does not support AES encryption for some reason
             throw new RuntimeException(exception);
         }
